5f8832d1c173f21c1dbfc983a65841776064dc11
AMITH B N <85832517+amith0608@users.noreply.github.com>
2023-07-16 21:07:22 +0530

    Files Initiated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Map from "./components/Map.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Choose Your Location and Capture</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BabylonMap.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef } from "react";
+import { Engine, Scene } from "react-babylonjs";
+import * as BABYLON from "babylonjs";
+import "babylonjs-loaders";
+import "../App.css";
+
+
+const BabylonMap = ({ capturedImages, onImageCaptured }) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    let engine = null;
+    let scene = null;
+
+    const createScene = (textures) => {
+      const newScene = new BABYLON.Scene(engine);
+      newScene.clearColor = new BABYLON.Color4(0, 0, 0, 0); // Set clearColor to transparent
+
+      const camera = new BABYLON.ArcRotateCamera(
+        "camera",
+        -Math.PI / 4,
+        Math.PI / 4,
+        8,
+        BABYLON.Vector3.Zero(),
+        newScene
+      );
+      camera.attachControl(canvasRef.current, true);
+      camera.wheelPrecision = 0; // Disable zooming with the mouse scroll
+      camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius; // Keep the camera at a fixed radius
+
+      const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        newScene
+      );
+
+      const box = BABYLON.MeshBuilder.CreateBox(
+        "box",
+        { width: 3, height: 3, depth: 3 },
+        newScene
+      );
+      const material = new BABYLON.StandardMaterial("material", newScene);
+      if (textures.length > 0) {
+        // Use the latest captured image URL as the texture
+        material.diffuseTexture = new BABYLON.Texture(textures[textures.length - 1].url, newScene);
+      }
+      box.material = material;
+
+      return newScene;
+    };
+
+    const initializeScene = () => {
+      const canvas = canvasRef.current;
+      const width = 200; // Set the desired width for the canvas
+
+      canvas.width = width; // Set the canvas width
+
+      engine = new BABYLON.Engine(canvas, true, {
+        preserveDrawingBuffer: true,
+        stencil: true,
+      });
+      scene = createScene(capturedImages);
+
+      engine = new BABYLON.Engine(canvasRef.current, true);
+      scene = createScene(capturedImages);
+
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
+
+      window.addEventListener("resize", () => {
+        engine.resize();
+      });
+    };
+
+    initializeScene();
+
+    return () => {
+      if (scene) {
+        scene.dispose();
+      }
+      if (engine) {
+        engine.dispose();
+      }
+    };
+  }, [capturedImages]);
+
+  return <canvas className="canvas-container" ref={canvasRef} tabIndex={0} />;
+};
+
+export default BabylonMap;
